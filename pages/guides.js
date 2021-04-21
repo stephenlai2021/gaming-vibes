f235d316b698fe1f0692bca433bbd8df37990472
stephenlaichaowen <stephenlaitest@gmail.com>
2021-04-21 17:29:03 +0800

    commit

---
+++ pages/guides.js
@@ -0,0 +1,16 @@
+export default function Guides() {
+  return (
+    <div className="guides">
+      <h2>All Guides</h2>
+
+      <style jsx>{`
+        .guides {
+          max-width: 960px;
+          margin: 40px auto;
+          letter-spacing: 1px;
+          line-height: 1.6em;
+        }
+      `}</style>
+    </div>
+  );
+}
