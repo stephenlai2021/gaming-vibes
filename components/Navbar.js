c6b2d7083f354f149f23cc7020cdf66635496eb2
stephenlaichaowen <stephenlaitest@gmail.com>
2021-04-26 22:04:43 +0800

    commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,28 +1,46 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { useContext } from 'react'
-import AuthContext from '../stores/authContext'
+import Link from "next/link";
+import Image from "next/image";
+import { useContext } from "react";
+import AuthContext from "../stores/authContext";
 
 export default function Navbar() {
-  const { user, login, logout } = useContext(AuthContext)
-  console.log(user)
+  const { user, login, logout, authReady } = useContext(AuthContext);
+  console.log(user);
 
   return (
     <div className="container">
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
-        <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/guides"><a>Guides</a></Link></li>
-          {!user && <li onClick={login} className="btn">Login/Signup</li>}
-          {user && <li>{ user.email }</li>}
-          {user && <li onClick={logout} className="btn">Log out</li>}
-        </ul>
+        {authReady && (
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/guides">
+                <a>Guides</a>
+              </Link>
+            </li>
+            {!user && (
+              <li onClick={login} className="btn">
+                Login/Signup
+              </li>
+            )}
+            {user && <li>{user.email}</li>}
+            {user && (
+              <li onClick={logout} className="btn">
+                Log out
+              </li>
+            )}
+          </ul>
+        )}
       </nav>
       <div className="banner">
         <Image src="/banner.png" width={966} height={276} />
       </div>
     </div>
-  )
+  );
 }
