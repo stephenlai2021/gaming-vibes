0634126dc9b7c4e275fa0bf288b9a2ea3fe5a8f7
stephenlaichaowen <stephenlaitest@gmail.com>
2021-04-29 20:26:36 +0800

    commit

--- hooks/use-netlify-function.js
+++ hooks/use-netlify-function.js
@@ -1,34 +1,7 @@
-import { useEffect, useState, useContext } from "react";
-import AuthContext from "../../stores/authContext";
+import React from 'react'
 
-export default function useNetliyFunction(function) {
-  const [guides, setGuides] = useState(null);
-  const { user, authReady } = useContext(AuthContext);
-  const [error, setError] = useState(null);
-
-  useEffect(async () => {
-    if (authReady) {
-      try {
-        const res = await fetch(
-          "/.netlify/functions/guides",
-          user && {
-            headers: {
-              Authorization: "Bearer " + user.token.access_token,
-            },
-          }
-        );
-        if (!res.ok) {
-          throw Error("You must be logged in to view this content");
-        }
-        const data = await res.json();
-        setGuides(data);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setGuides(null);
-      }
-    }
-  }, [user, authReady]);
-
-  return { guides, user, error }
+const UseNetlifyFunction = () => {
+  return { msg: 'hi, there !' }
 }
+
+export default UseNetlifyFunction
--- pages/guides/[guideSlug].jsx
+++ pages/guides/[guideSlug].jsx
@@ -3,23 +3,8 @@ import AuthContext from "../../stores/authContext";
 import { useRouter } from "next/router";
 
 const GuideDetails = () => {
-
-  // useEffect(async () => {
-  //   const res = await fetch(
-  //     "/.netlify/functions/guides",
-  //     user && {
-  //       headers: {
-  //         Authorization: "Bearer " + user.token.access_token,
-  //       },
-  //     }
-  //   );
-  //   if (!res.ok) {
-  //     throw Error("You must be logged in to view this content");
-  //   }
-  //   const data = await res.json();
-  //   console.log(data);
-  // }, []);
-
+  const router = useRouter()
+  
   return <div>ID: {router.query.guideSlug}</div>;
 };
 
